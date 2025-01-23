e2384ada9e9689b7ce776c7f02058ebe308fb09a
Gaurav Karemore <95206483+gauravrajlaxmi@users.noreply.github.com>
2025-01-23 23:40:21 +0530

    Update worker.service.ts

--- angular-frontend/src/app/services/worker.service.ts
+++ angular-frontend/src/app/services/worker.service.ts
@@ -9,7 +9,7 @@ import { Worker } from '../models/worker';
 })
 export class WorkerService {
 
-  private getUrl: string = "http://localhost:8080/api/v1/workers";
+  private getUrl: string = "http://54.83.90.121:8080/api/v1/workers";
 
   constructor(private _httpClient: HttpClient) { }
 
